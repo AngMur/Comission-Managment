9fbddb5209b32a3c42e9b094ad154923fd9a0dc5
AngMur <rodolfoyeih@gmail.com>
2026-02-12 11:51:03 -0600

    Render static files

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+
 const app = express();
 
 app.use(cors());
@@ -17,6 +18,14 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, 'views'), {
+    extensions: ['html']
+}));
+
+app.get('/', (req, res) => {
+    res.redirect('/login');
 });
 
 const userRoutes = require('./routes/userRoutes');
